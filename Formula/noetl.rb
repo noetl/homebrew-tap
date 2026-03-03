3950da228b1d9352d93ec670ae57479bc97255c3
Kadyapam <182583029+kadyapam@users.noreply.github.com>
2026-03-02 19:38:50 -0800

    noetl 2.8.7

--- Formula/noetl.rb
+++ Formula/noetl.rb
@@ -5,10 +5,11 @@ class Noetl < Formula
   sha256 "df3bcc8e0b56c59c257ce51804e29693916e53f34de760969bb38ed379337dd7"
   license "MIT"
   head "https://github.com/noetl/cli.git", branch: "main"
+
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", "--bins", *std_cargo_args
+    system "cargo", "install", "--path", ".", "--bins", *std_cargo_args
   end
 
   test do
