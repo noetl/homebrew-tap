b6b1bc13e772c38986dbed5fc002de12b9477143
Kadyapam <182583029+kadyapam@users.noreply.github.com>
2026-03-29 23:23:45 -0700

    noetl 2.13.0

--- Formula/noetl.rb
+++ Formula/noetl.rb
@@ -1,8 +1,8 @@
 class Noetl < Formula
   desc "NoETL workflow automation CLI - Execute playbooks locally or orchestrate distributed pipelines"
   homepage "https://noetl.io"
-  url "https://github.com/noetl/cli/archive/refs/tags/v2.8.7.tar.gz"
-  sha256 "df3bcc8e0b56c59c257ce51804e29693916e53f34de760969bb38ed379337dd7"
+  url "https://github.com/noetl/cli/archive/refs/tags/v2.13.0.tar.gz"
+  sha256 "bafd772dc0cb5b02efb16b4afd3fd65a81ee2b6adbf130538c428febd76d7bca"
   license "MIT"
   head "https://github.com/noetl/cli.git", branch: "main"
 
